d1282cc2b9b84818884fa2a996db91e56be6058f
Phuxiuss <xphuxiusplays@tutanota.com>
2026-01-29 21:19:19 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,10 +2,11 @@
 echo Setting up SFML project...
 
 REM Copy SFML DLLs to Debug folder
-xcopy /y External\SFML\bin\*.dll CalculatorSFML\bin\Debug\
+xcopy /y External\SFML-3.0.2\bin\*.dll CalculatorSFML\bin\Debug\
 
 REM Open the solution
 start CalculatorSFML.sln
 
-echo Done!
-pause
+echo Done! Closing in 3 seconds...
+timeout /t 3 /nobreak >nul
+exit
